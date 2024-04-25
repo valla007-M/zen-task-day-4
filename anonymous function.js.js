55b54612158ca48954edd149885a745a19a0afd7
valla007-M <166891615+valla007-M@users.noreply.github.com>
2024-04-25 15:34:05 +0530

    Add files via upload
    Arrow functions are handy for simple actions, especially for one-liners. They come in two flavour: Without curly braces: (...args) => expression – the right side is an expression

---
+++ anonymous function.js.js
@@ -0,0 +1,57 @@
+//print odd number in an array
+function(array) {
+    for (var i= 0 ; i < array.leghth ;i++){
+        if(array)[i]2%!=0){
+            console.log(array[i])
+        }
+    }
+
+
+}
+(function(array)){
+    for(var i=0 i< array.lenghth;i++){
+        if(array[i]%2!=0){
+            console.log(array[i])
+       
+        })([1,2,3,4])
+    }
+
+}
+
+//convert all string to title caps in a string array
+
+function(str){
+    str=str.tolowercase().split("")
+    for(var i=0;i<str.lenghth;i++)
+{
+str[i].charat(0).touppercase()+str.slice(1)
+}
+return str.join('');
+}
+
+let (function(str)){
+    str=str.lowercase().split("");
+    for(var i)
+}
+
+
+arrow function    odd numbers=array=> {
+    for (var i=0;i< array.length; i++){
+        if(array[i]%2!=0){
+            console.log(array[i])
+        }
+    }
+}
+
+//convert all stringto little caps in a string array
+
+function(str){
+    str=str.toloercase().split("");
+    for(vari=0;i<str.leghth;i++)
+}
+str[i]=str[i].charat(0).touppercase()+str[i].slice(1);
+
+
+
+
+
